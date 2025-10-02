1666885eecb4cfe94dab7242d50cb93b45eb798f
Arif Khan <786arifkhan0@gmail.com>
2025-10-02 11:58:37 +0000

    commit

---
+++ models/employees_salary.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='table') 
+    }}
+
+
+with
+emp_data as (
+    select id,
+           name, 
+           salary,
+           tax_amount,
+           ({{calculate_amount('salary','tax_amount')}}) as total_amount
+     from 
+    {{source('datafeed_shared_schema','EMP_SAL')}}
+)
+select * from emp_data
